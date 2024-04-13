46b6ff6fe5a407cb816907cab34805d5f366e56b
Choi Minkyu (mc76728) <mc76728@austin.utexas.edu@ece-a52150.austin.utexas.edu>
2024-04-13 15:44:27 +0000

    update version

--- src/caggpu/__init__.py
+++ src/caggpu/__init__.py
@@ -5,4 +5,4 @@ from __future__ import annotations
 from .nvidia_cuda import get_free_gpus, set_cuda_visible_devices
 
 __all__ = ["get_free_gpus", "set_cuda_visible_devices"]
-__version__ = "0.0.1"
+__version__ = "0.0.2"
